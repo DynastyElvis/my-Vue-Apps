2eb4147c5c9301087642c9f297733757eea9fda9
kipkemoilvs <kipkemoilvs@gmail.com>
2023-05-27 09:54:16 +0300

    ft:instalation bugs

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,3 +1,10 @@
 <template>
-    <h1>MyHeader</h1>
+    <div>
+        <div>
+            <b-nav>
+              <b-nav-item active>Quiz App</b-nav-item>
+              <b-nav-item>Counter: 4/10</b-nav-item>
+            </b-nav>
+          </div>
+    </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the
 createApp(App).mount('#app')
